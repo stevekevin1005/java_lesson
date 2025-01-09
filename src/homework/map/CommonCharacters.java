87d0c6d6bef200b57593b4ce9f1b592ad5940b25
charLin <118043579+Code-Min159@users.noreply.github.com>
2025-01-09 14:35:59 +0800

    Update CommonCharacters.java

--- src/homework/map/CommonCharacters.java
+++ src/homework/map/CommonCharacters.java
@@ -4,7 +4,15 @@ public class CommonCharacters {
     public static void main(String[] args) {
         String str1 = "aabbcc";
         String str2 = "abcc";
+        //合併字串
+        String str = str1.concat(str2);
 
+        //map 特性 有重複
+        //set 特性 沒有重複
+        SET<String> str = new MAP<>();
+        for(String str: str){
+            str.add(str);
+        }
         // 預期結果 {a=1, b=1, c=2}
         System.out.println("");
     }
